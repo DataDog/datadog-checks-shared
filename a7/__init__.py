88cdd35e18b1fd4500d53936a2e02f7c4debdfdb
Ofek Lev <ofekmeister@gmail.com>
2019-01-03 15:15:27 -0500

    Fix unicode & stream flushing

--- a7/__init__.py
+++ a7/__init__.py
@@ -11,12 +11,14 @@ from .validate import validate_py3
 def main():
     """Run validate on a single python file"""
     if len(sys.argv) != 2:
-        sys.stderr.write("No python file specified")
+        sys.stderr.write("No python file specified.\n")
+        sys.stderr.flush()
         sys.exit(1)
 
-    file_path = os.path.abspath(os.path.realpath(sys.argv[1]))
+    file_path = os.path.realpath(sys.argv[1].decode(sys.getfilesystemencoding()))
     if not os.path.exists(file_path):
-        sys.stderr.write("{} does not exist.".format(file_path))
+        sys.stderr.write(u"{} does not exist.\n".format(file_path))
+        sys.stderr.flush()
         sys.exit(1)
 
     results = validate_py3(file_path)
