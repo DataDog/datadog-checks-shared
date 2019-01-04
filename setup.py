92b5db78a28113a3b3495800ced7c24431774810
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2019-01-04 11:42:20 +0100

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), 'r', encoding='utf-8') as f:
 
 setup(
     name='datadog-a7',
-    version="0.0.3",
+    version="0.0.4",
 
     description='The Datadog Checks Shared Tools',
     long_description=README,
